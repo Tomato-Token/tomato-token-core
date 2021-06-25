181b9a6e72ea423299974818e67fab751a72c28a
killertomato69 <killertomato69@protonmail.com>
2021-06-25 17:22:56 -0500

    lower gas

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -69,7 +69,7 @@ const config: HardhatUserConfig = {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts,
-      gasPrice: 120 * 1000000000,
+      gasPrice: 30 * 1000000000,
       chainId: 1,
     },
     localhost: {
